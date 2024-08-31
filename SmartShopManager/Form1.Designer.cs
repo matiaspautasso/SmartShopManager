9cc3028ae5a036ff4170a682dd2dfc9406e71148
Matias Pautasso <matiaspautasso@outlook.com>
2024-08-31 20:02:31 -0300

    Agregar archivos de proyecto.

---
+++ SmartShopManager/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SmartShopManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvShow = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboCat = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboProd = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstShow = new System.Windows.Forms.ListBox();
+            this.btnADD = new System.Windows.Forms.Button();
+            this.btnBuy = new System.Windows.Forms.Button();
+            this.lblTot = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvShow
+            // 
+            this.dgvShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShow.Location = new System.Drawing.Point(484, 53);
+            this.dgvShow.Name = "dgvShow";
+            this.dgvShow.Size = new System.Drawing.Size(451, 359);
+            this.dgvShow.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.lstShow);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cboProd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cboCat);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(36, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(348, 410);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "shop";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "category";
+            // 
+            // cboCat
+            // 
+            this.cboCat.FormattingEnabled = true;
+            this.cboCat.Location = new System.Drawing.Point(117, 41);
+            this.cboCat.Name = "cboCat";
+            this.cboCat.Size = new System.Drawing.Size(121, 21);
+            this.cboCat.TabIndex = 1;
+            this.cboCat.SelectedIndexChanged += new System.EventHandler(this.cboCat_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "product";
+            // 
+            // cboProd
+            // 
+            this.cboProd.FormattingEnabled = true;
+            this.cboProd.Location = new System.Drawing.Point(117, 111);
+            this.cboProd.Name = "cboProd";
+            this.cboProd.Size = new System.Drawing.Size(121, 21);
+            this.cboProd.TabIndex = 3;
+            this.cboProd.SelectedIndexChanged += new System.EventHandler(this.cboProd_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "description";
+            // 
+            // lstShow
+            // 
+            this.lstShow.FormattingEnabled = true;
+            this.lstShow.Location = new System.Drawing.Point(34, 253);
+            this.lstShow.Name = "lstShow";
+            this.lstShow.Size = new System.Drawing.Size(283, 134);
+            this.lstShow.TabIndex = 5;
+            // 
+            // btnADD
+            // 
+            this.btnADD.Location = new System.Drawing.Point(26, 455);
+            this.btnADD.Name = "btnADD";
+            this.btnADD.Size = new System.Drawing.Size(221, 47);
+            this.btnADD.TabIndex = 2;
+            this.btnADD.Text = "Add ";
+            this.btnADD.UseVisualStyleBackColor = true;
+            this.btnADD.Click += new System.EventHandler(this.btnADD_Click);
+            // 
+            // btnBuy
+            // 
+            this.btnBuy.Location = new System.Drawing.Point(266, 455);
+            this.btnBuy.Name = "btnBuy";
+            this.btnBuy.Size = new System.Drawing.Size(221, 47);
+            this.btnBuy.TabIndex = 3;
+            this.btnBuy.Text = "buy";
+            this.btnBuy.UseVisualStyleBackColor = true;
+            // 
+            // lblTot
+            // 
+            this.lblTot.AutoSize = true;
+            this.lblTot.Location = new System.Drawing.Point(846, 488);
+            this.lblTot.Name = "lblTot";
+            this.lblTot.Size = new System.Drawing.Size(19, 13);
+            this.lblTot.TabIndex = 4;
+            this.lblTot.Text = "tot";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(117, 175);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 569);
+            this.Controls.Add(this.lblTot);
+            this.Controls.Add(this.btnBuy);
+            this.Controls.Add(this.btnADD);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvShow;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstShow;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboProd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboCat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnADD;
+        private System.Windows.Forms.Button btnBuy;
+        private System.Windows.Forms.Label lblTot;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ SmartShopManager/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartShopManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void CompleteCbo() 
+        {
+            cboCat.Items.Clear();
+            List<string> categoriasFiltradas = new List<string>();
+
+            products.ForEach(elementos =>
+            {
+                if (!categoriasFiltradas.Contains(elementos["cate"]))
+                {
+                   
+                    categoriasFiltradas.Add(elementos["cate"].ToString());
+                }
+            });
+
+            categoriasFiltradas.ForEach(elem =>
+            {
+                cboCat.Items.Add(elem.ToString());
+            });
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CompleteCbo();
+           columns.ForEach(elementos =>
+                  {
+                dgvShow.Columns.Add($"col{elementos}", elementos.ToString());
+                   });
+        }
+        List<string> columns = new List<string>() { "Product", "Quantity", "Tot" };
+        List<string> categories = new List<string>() { "Clothes", "Electronics", "Home" };
+
+         List<Dictionary<string, object>> products = new List<Dictionary<string, object>>() {
+            new Dictionary<string, object> { { "name", "Shirt" }, { "cate", "Clothes" }, { "price", 123 }, { "desc", "Gray shirt" } },
+           new Dictionary<string, object> { { "name", "Pants" }, { "cate", "Clothes" }, { "price", 150 }, { "desc", "Casual pants" } },
+           new Dictionary<string, object> { { "name", "Jacket" }, { "cate", "Clothes" }, { "price", 200 }, { "desc", "Windbreaker jacket" } },
+
+            new Dictionary<string, object> { { "name", "Keyboard" }, { "cate", "Electronics" }, { "price", 80 }, { "desc", "Mechanical keyboard" } },
+             new Dictionary<string, object> { { "name", "Headphones" }, { "cate", "Electronics" }, { "price", 75 }, { "desc", "Wireless headphones" } },
+                 new Dictionary<string, object> { { "name", "Mouse pad" }, { "cate", "Electronics" }, { "price", 50 }, { "desc", "LED-lit mouse pad" } },
+
+                 new Dictionary<string, object> { { "name", "Electric Oven" }, { "cate", "Home" }, { "price", 300 }, { "desc", "Small electric oven" } },
+               new Dictionary<string, object> { { "name", "Robot Vacuum" }, { "cate", "Home" }, { "price", 250 }, { "desc", "Quiet with fast charging" } },
+             new Dictionary<string, object> { { "name", "40'' LED TV" }, { "cate", "Home" }, { "price", 200 }, { "desc", "40'' Smart TV" } },
+
+               new Dictionary<string, object> { { "name", "Football Boots" }, { "cate", "Sports" }, { "price", 300 }, { "desc", "Small electric oven" } },
+            new Dictionary<string, object> { { "name", "Football" }, { "cate", "Sports" }, { "price", 250 }, { "desc", "Quiet with fast charging" } },
+               new Dictionary<string, object> { { "name", "Boxing Gloves" }, { "cate", "Sports" }, { "price", 200 }, { "desc", "40'' Smart TV" } },
+          };
+
+        private void cboProd_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstShow.Items.Clear();
+            string selecProd = cboProd.SelectedItem.ToString();
+
+            int index = products.FindIndex(elem => elem["name"].Equals(selecProd));
+
+            lstShow.Items.Add($"name: {products[index]["name"]}");
+            lstShow.Items.Add($"desc: {products[index]["desc"]}");
+            lstShow.Items.Add($"price: ${products[index]["price"]}");
+        }
+
+        private void cboCat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cboProd.Items.Clear();
+            string cateSeleccionada = cboCat.SelectedItem.ToString();
+
+            products.ForEach(elem =>
+            {
+                if (elem["cate"].Equals(cateSeleccionada))
+                {
+                    cboProd.Items.Add(elem["name"].ToString());
+                }
+            });
+        }
+        List<Dictionary<string, object>> car = new List<Dictionary<string, object>>();
+        private void btnADD_Click(object sender, EventArgs e)
+        {
+            string selectedProd = cboProd.SelectedItem.ToString();
+            int cant = Convert.ToInt32(numericUpDown1.Value);
+            int index = products.FindIndex(elem => elem["name"].Equals(selectedProd));
+            int ultimoIndex = 0;
+            int total = 0;
+
+            Dictionary<string, object> aux = new Dictionary<string, object>();
+
+            aux["name"] = products[index]["name"];
+            aux["Quantity"] = cant;
+            aux["price"] = products[index]["price"];
+
+            car.Add(aux);
+
+            ultimoIndex = car.Count - 1;
+
+            dgvShow.Rows.Add(car[ultimoIndex]["name"], car[ultimoIndex]["Quantity"], (int)car[ultimoIndex]["price"] * (int)car[ultimoIndex]["Quantity"]);
+
+            car.ForEach(elem =>
+            {
+                total += (int)elem["price"] * (int)elem["Quantity"];
+            });
+
+            lblTot.Text = $"TOT: ${total}";
+        }
+    }
+}
